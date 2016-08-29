e31f7a92c51c567e2e049898eed224eb68987caa
Jiapo <jiapoahu@gmail.com>
2016-08-29 15:17:43 +0800

    串口通信

---
+++ Serial/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxBaud = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(211, 224);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(64, 23);
+            this.BtnSend.TabIndex = 0;
+            this.BtnSend.Text = "发送";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            this.BtnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.LightGray;
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnOpen.Location = new System.Drawing.Point(281, 9);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(65, 12);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(57, 20);
+            this.comboBoxPort.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SandyBrown;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.SandyBrown;
+            this.label2.Location = new System.Drawing.Point(140, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "BaudRate：";
+            // 
+            // comboBoxBaud
+            // 
+            this.comboBoxBaud.FormattingEnabled = true;
+            this.comboBoxBaud.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaud.Location = new System.Drawing.Point(211, 11);
+            this.comboBoxBaud.Name = "comboBoxBaud";
+            this.comboBoxBaud.Size = new System.Drawing.Size(57, 20);
+            this.comboBoxBaud.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 21);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 38);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(333, 171);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "清空";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(367, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxBaud);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.BtnSend);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxBaud;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Serial/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Serial
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort McuPort = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();//获取本机串口数组
+            Array.Sort(ports);//对串口排序
+            comboBoxPort.Items.AddRange(ports);//添加到下拉列表
+            comboBoxPort.SelectedIndex = comboBoxPort.Items.Count > 0 ? 0 : -1;
+            comboBoxBaud.SelectedIndex = comboBoxBaud.Items.IndexOf("9600");
+            textBox2.Text = "串口" + comboBoxPort.Text + "初始化完成" + "\r\n等待打开";
+            McuPort.DataReceived += Port_DataReceived;
+        }
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int n = McuPort.BytesToRead;
+            byte[] buf = new byte[n];
+            McuPort.Read(buf, 0, n);
+            textBox2.AppendText("\r\n"+Encoding.ASCII.GetString(buf));
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (McuPort.IsOpen)
+            {
+                McuPort.Close();
+            }
+
+            else
+            {
+                McuPort.PortName = comboBoxPort.Text;
+                McuPort.BaudRate = int.Parse(comboBoxBaud.Text);
+                try
+                {
+                    McuPort.Open();
+                    textBox2.Text = "串口已打开";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            
+            btnOpen.Text = McuPort.IsOpen ? "关闭" : "打开";
+            
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            McuPort.Write(textBox1.Text);
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e) 
+        {   
+            if (e.KeyCode == Keys.Enter)   
+            {
+                this.BtnSend_Click(sender, e);//触发按钮事件
+            } 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = null;
+        }
+    }
+}
